5c7b8d7c9e1afda3eeb3cf40dad7ff44ffee8d8a
TruelyMostWanted <45302427+TruelyMostWanted@users.noreply.github.com>
2025-01-27 00:01:33 +0100

    Added initialize.sql

---
+++ portfolio_uebung2/initialize.sql
@@ -0,0 +1,12 @@
+﻿-- initialize.sql
+DROP TABLE IF EXISTS Wishes;
+
+CREATE TABLE IF NOT EXISTS Wishes (
+    Id INT AUTO_INCREMENT,
+    Description VARCHAR(500) NOT NULL,
+    FileName VARCHAR(100),
+    Status ENUM('Formulated', 'InProgress', 'Delivering', 'UnderTree') NOT NULL,
+    PRIMARY KEY (Id)
+    );
+
+INSERT INTO Wishes (Description, FileName, Status) VALUES ('I want a new bike', '', 'Formulated');
